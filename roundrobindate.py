2c303d993265f461b028c1db12542568b596d586
Chris Laskey <contact@chrislaskey.com>
2013-01-29 15:18:27 -0500

    Sketching out architecture and tests.

--- roundrobindate.py
+++ roundrobindate.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python
 
+from datetime import datetime, date, timedelta
+
 class RoundRobinDate:
 
     def __init__(self, options=""):
-        # self._parse_options(options)
-        pass
+        self.options = self._get_default_options()
+        # self.set_options(options)
 
     def __str__(self):
-        """ Display current options """
+        "Display current options"
         return "Hello World"
 
-    # def _return_current_datetime(self):
-    #     """ Abstracted current datetime to aid testing by inheritence. """
-    #     pass
+    def _get_days_in_month(self, month, year=""):
+        "Get days by subtracting a day from the beginning of next month"
+        next_month = self._get_next_month(month)
+        if not year:
+            year = date.today().year
+        last_day_of_month = date(year, next_month, 1) - timedelta(days=1)
+        return last_day_of_month.day
+
+    def _get_next_month(self, month):
+        if month == 12:
+            return 1
+        else:
+            return month + 1
+
+    def _get_previous_month(self, month):
+        # TODO: Depricated
+        if month == 1:
+            return 12
+        else:
+            return month - 1
+
+    def _get_current_date(self):
+        "Abstracted current date to aid testing by inheritence"
+        return date.now()
 
     # def _parse_datetime(self, options):
     #     pass
@@ -20,22 +43,43 @@ class RoundRobinDate:
     # def generate(self, start_date=""):
     #     pass
 
-    # def _parse_options(self, options):
-    #     if not options:
-    #         options = self._get_default_options()
         
-    # def _get_default_options(self):
-    #     default_options = {
-    #         "start_date": self.datetime
-        
-    #     }
-    #     return default_options
+    def _get_default_options(self):
+        default_options = {
+            "start_date": date.today(),
+            "days_to_retain": 7,
+            "weeks_to_retain": 3,
+            "months_to_retain": '6',
+            "years_to_retain": 20
+        }
+        return default_options
 
-    # def _setup_datetime(self, options):
-    #     pass
+    # def set_options(self, new_options):
+    #     """
+    #     Update options, accepts either complete or partial options dictionary.
+    #     If partial the new options are merged with current options dictionary.
+    #     """
+    #     if new_options:
+    #         self.options.update(new_options)
+    #         self._parse_options()
 
-    # def set_options(self, options):
-    #     self._parse_options(options)
+    # def _parse_options(self):
+    #     self._parse_start_date_options()
+    #     self._parse_retain_options()
 
-    # def get_options(self):
-    #     return self.__str__()
+    # def _parse_start_date_options(self):
+    #     start_date = self.options.get("start_date")
+    #     if not start_date:
+    #         raise Exception("Invalid option for 'start_date': cannot be empty")
+    #     if not isinstance(start_date, date):
+    #         start_date_as_date = date(start_date)
+    #         self.options["start_date"] = start_date_as_date
+
+    # def _parse_retain_options(self):
+    #     key_prefixes = ["days", "weeks", "months", "years"]
+    #     keys = [ x + "_to_retain" for x in key_prefixes ]
+    #     updated_values = { key: int(value) for (key, value) in self.options.iteritems() if value in keys }
+    #     self.options.update(updated_values)
+
+    def get_options(self):
+        return self.options
--- tests/test-roundrobindate.py
+++ tests/test-roundrobindate.py
@@ -1,13 +1,31 @@
 # -*- coding: utf8 -*-
 
+# nosetests --with-coverage --cover-package=roundrobindate ./tests
+
 from nose.tools import *
 from roundrobindate import RoundRobinDate
+from datetime import datetime, date, timedelta
+
+class RoundRobinDateTestingChild(RoundRobinDate):
+    """
+    Identical to RoundRobinDate with additional ability to manipulate the date
+    value.
+    """
+    def __init__(self, options=""):
+        RoundRobinDate.__init__(self, options)
+
+    def _get_current_date(self):
+        " Abstracted current date to aid testing by inheritence. "
+        return self._testing_date
+
+    def set_current_date(self, year, month, day):
+        self._testing_date = date(year, month, day)
 
 class TestRoundRobinDate():
 
     def setup(self):
         "Set up test fixtures"
-        self.rrd = RoundRobinDate()
+        self.rrd = RoundRobinDateTestingChild()
 
     def teardown(self):
         "Tear down test fixtures"
@@ -18,11 +36,55 @@ class TestRoundRobinDate():
         result = self.rrd.__str__()
         assert_equal(result, expected)
 
-    # def test_default_options(self):
-    #     "Test the default options"
-    #     expected = {}
-    #     result = self.rrd.get_options()
-    #     assert_equal(result, expected)
+    def test__get_next_month(self):
+        expected = 2
+        result = self.rrd._get_next_month(1)
+        assert_equal(result, expected)
+
+        expected = 1
+        result = self.rrd._get_next_month(12)
+        assert_equal(result, expected)
+
+    def test__get_previous_month(self):
+        expected = 12
+        result = self.rrd._get_previous_month(1)
+        assert_equal(result, expected)
+
+        expected = 1
+        result = self.rrd._get_previous_month(2)
+        assert_equal(result, expected)
+
+    def test_set_current_date(self):
+        self.rrd.set_current_date(2010,01,05)
+        result = self.rrd._get_current_date()
+        assert_equal(result.day, 5)
+        assert_equal(result.month, 1)
+        assert_equal(result.year, 2010)
+
+    def test__get_days_in_month(self):
+        result = self.rrd._get_days_in_month(1, 2010)
+        assert_equal(result, 31)
+
+        result = self.rrd._get_days_in_month(4, 2010)
+        assert_equal(result, 30)
+
+        result = self.rrd._get_days_in_month(2, 2010)
+        assert_equal(result, 28)
+
+        result = self.rrd._get_days_in_month(2, 2012)
+        assert_equal(result, 29)
+
+        try:
+            result = self.rrd._get_days_in_month("4", "2010")
+            assert False
+        except TypeError:
+            assert True
+
+    def test_default_options(self):
+        "Test the default options"
+        expected = self.rrd._get_default_options()
+        result = self.rrd.get_options()
+        assert_equal(result, expected)
 
     # def test_set_option(self):
     #     "Test setting a basic option"
