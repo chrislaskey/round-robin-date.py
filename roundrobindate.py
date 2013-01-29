e59c7ad00ce5425b43bdc2e72cade34def693ff1
Chris Laskey <contact@chrislaskey.com>
2013-01-28 22:47:34 -0500

    Sketching potential interfaces and high level implementations

--- roundrobindate.py
+++ roundrobindate.py
@@ -1,12 +1,41 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
 class RoundRobinDate:
 
-    def __init__(self):
+    def __init__(self, options=""):
+        # self._parse_options(options)
         pass
 
-    def hello_world(self):
+    def __str__(self):
+        """ Display current options """
         return "Hello World"
+
+    # def _return_current_datetime(self):
+    #     """ Abstracted current datetime to aid testing by inheritence. """
+    #     pass
+
+    # def _parse_datetime(self, options):
+    #     pass
+
+    # def generate(self, start_date=""):
+    #     pass
+
+    # def _parse_options(self, options):
+    #     if not options:
+    #         options = self._get_default_options()
+        
+    # def _get_default_options(self):
+    #     default_options = {
+    #         "start_date": self.datetime
+        
+    #     }
+    #     return default_options
+
+    # def _setup_datetime(self, options):
+    #     pass
+
+    # def set_options(self, options):
+    #     self._parse_options(options)
+
+    # def get_options(self):
+    #     return self.__str__()
--- tests/test-roundrobindate.py
+++ tests/test-roundrobindate.py
@@ -3,16 +3,38 @@
 from nose.tools import *
 from roundrobindate import RoundRobinDate
 
-def setup():
-    "set up test fixtures"
+class TestRoundRobinDate():
 
-def teardown():
-    "tear down test fixtures"
+    def setup(self):
+        "Set up test fixtures"
+        self.rrd = RoundRobinDate()
 
-@with_setup(setup, teardown)
-def test_hello_world():
-    "throwaway test to establish hello world with application class."
-    rrdate = RoundRobinDate()
-    result = rrdate.hello_world()
-    expected = "Hello World"
-    assert_equal(result, expected)
+    def teardown(self):
+        "Tear down test fixtures"
+
+    def test_string_representation(self):
+        "Test the class string representation via the magic method __str__"
+        expected = "Hello World"
+        result = self.rrd.__str__()
+        assert_equal(result, expected)
+
+    # def test_default_options(self):
+    #     "Test the default options"
+    #     expected = {}
+    #     result = self.rrd.get_options()
+    #     assert_equal(result, expected)
+
+    # def test_set_option(self):
+    #     "Test setting a basic option"
+
+    # def test_set_empty_option(self):
+    #     "Test setting a option to empty values"
+    #     expected = {}
+    #     options = self.rrd.get_options()
+    #     assert_equal(result, expected)
+
+    # def test_set_complex_option(self):
+    #     "Test setting a option to collections"
+    #     expected = {}
+    #     options = self.rrd.get_options()
+    #     assert_equal(result, expected)
